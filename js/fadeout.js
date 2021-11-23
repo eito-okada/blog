159f83803802948fab985f1250a12bda3df2b3c1
eito-okada <eito8okada@gmail.com>
2021-11-24 06:13:26 +0900

    commit

--- js/fadeout.js
+++ js/fadeout.js
@@ -14,12 +14,12 @@ $(function(){
     $(document).scroll(function() {
       footerdistance = $(this).scrollTop();
       if (footerscroll <= footerdistance) {
-        $('.footer-logo').css({
+        $('.footer-logo-link').css({
           transform: 'scale(1.5)',
           transition: '1s ease'
         })
       } else {
-        $('.footer-logo').css( {
+        $('.footer-logo-link').css( {
           transform:'scale(1)',
           transition: '1s ease-in-out'
         })
